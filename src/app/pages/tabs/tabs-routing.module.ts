91ca8effb2c6f9b28202194567f3d617bc4382f7
Ovidiojcf <ojcfarias@gmail.com>
2024-02-22 09:59:21 -0300

    first commit

---
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: 'cart',
+        loadChildren: () =>
+          import('./cart/cart.module').then((m) => m.CartPageModule),
+      },
+      {
+        path: 'search',
+        loadChildren: () =>
+          import('./search/search.module').then((m) => m.SearchPageModule),
+      },
+      {
+        path: 'account',
+        loadChildren: () =>
+          import('./account/account.module').then((m) => m.AccountPageModule),
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'restaurants/:restaurantId',
+        loadChildren: ()=> import('./itens/itens.module').then( m => m.ItensPageModule)
+      }
+    ],
+  },
+  {
+    path: 'itens',
+    loadChildren: () => import('./itens/itens.module').then( m => m.ItensPageModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
